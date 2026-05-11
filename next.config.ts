353aab9442c5135e9ccd8b84bb802fa62cd63428
Chaminwoo <chaalsdn0217@naver.com>
2026-05-11 17:49:03 +0900

    FIX : server supabase link

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,25 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+
+      // 로컬/기존 supabase
       {
         protocol: 'https',
         hostname: 'hsajeiozrjtgeosytoss.supabase.co',
-        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+
+      // 서버용 supabase
+      {
+        protocol: 'https',
+        hostname: 'mkfscsovdmvpsctankpt.supabase.co',
         pathname: '/storage/v1/object/public/**',
       },
     ],
